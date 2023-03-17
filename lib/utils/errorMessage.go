7d78a30d4ddd6b7ef2194cbda822cbdbe3f21a5a
asidikfauzi <asidikfauzi@gmail.com>
2023-03-17 20:43:46 +0700

    error message

--- lib/utils/errorMessage.go
+++ lib/utils/errorMessage.go
@@ -1,1 +1,23 @@
 package utils
+
+import "github.com/go-playground/validator/v10"
+
+type ErrorMessageEmpty struct {
+	Field   string `json:"field"`
+	Message string `json:"message"`
+}
+
+func GetErrorMessageEmpty(fe validator.FieldError) string {
+	switch fe.Tag() {
+	case "required":
+		return "This field is required"
+	case "lte":
+		return "Should be less than " + fe.Param()
+	case "gte":
+		return "Should be greater than " + fe.Param()
+	case "oneof":
+		return "these fields should be user and admin"
+	}
+
+	return "Unknown error"
+}
