29518eb4a2533da197734c7506d34753fe62237e
asidikfauzi <asidikfauzi@gmail.com>
2023-03-17 15:59:56 +0700

    roles models

--- models/roles.go
+++ models/roles.go
@@ -1,1 +1,11 @@
 package models
+
+import "time"
+
+type Roles struct {
+	ID        int        `gorm:"primarykey;AUTO_INCREMENT" json:"id"`
+	Name      string     `gorm:"type:varchar(10);not null" json:"name"`
+	CreatedAt time.Time  `gorm:"default:null"`
+	UpdatedAt time.Time  `gorm:"default:null"`
+	DeleteAt  *time.Time `gorm:"default:null"`
+}
