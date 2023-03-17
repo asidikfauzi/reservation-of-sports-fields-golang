2083958c745d7f705c716c49c1deb141f33b92ea
asidikfauzi <asidikfauzi@gmail.com>
2023-03-17 20:44:06 +0700

    password

--- lib/utils/password.go
+++ lib/utils/password.go
@@ -1,1 +1,38 @@
 package utils
+
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func ValidatePassword(password string) bool {
+	if len(password) < 8 {
+		return false
+	}
+
+	var (
+		hasUpper  = false
+		hasLower  = false
+		hasNumber = false
+	)
+
+	for _, char := range password {
+		switch {
+		case 'A' <= char && char <= 'Z':
+			hasUpper = true
+		case 'a' <= char && char <= 'z':
+			hasLower = true
+		case '0' <= char && char <= '9':
+			hasNumber = true
+		}
+	}
+
+	return hasUpper && hasLower && hasNumber
+}
