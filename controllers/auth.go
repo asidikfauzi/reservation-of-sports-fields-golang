6da1e610a5b7b534a4347de6a9cc42e5331f8129
Fauzi Vorta <fauzi@vorta.live>
2023-03-22 01:19:44 +0700

    register

--- controllers/auth.go
+++ controllers/auth.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"asidikfauzi/reservation-of-sport-fields-golang/config"
 	"asidikfauzi/reservation-of-sport-fields-golang/lib/database"
 	"asidikfauzi/reservation-of-sport-fields-golang/lib/utils"
 	"asidikfauzi/reservation-of-sport-fields-golang/models"
@@ -8,8 +9,13 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"net/http"
+	"regexp"
 	"time"
 )
+
+var user []models.Users
+
+var emailRegex = regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
 
 func RegisterController(c *gin.Context) {
 	var userModel models.Users_register
@@ -30,33 +36,34 @@ func RegisterController(c *gin.Context) {
 		return
 	}
 
-	//if err := c.BindJSON(&userModel); err != nil {
-	//	c.JSON(http.StatusBadRequest, gin.H{
-	//		"code":    400,
-	//		"message": "Error registration users",
-	//		"status":  err.Error(),
-	//	})
-	//	return
-	//}
-
-	if err := models.Validate.Struct(userModel); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"code":    400,
-			"message": "Error registration users",
-			"status":  err.Error(),
-		})
+	if !emailRegex.MatchString(userModel.Email) {
+		utils.BadRequest(c, "Invalid email format")
 		return
 	}
 
 	if !utils.ValidatePassword(userModel.Password) {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"code": 400,
-			"message": "Password must be at least 8 " +
-				"characters long and contain " +
-				"at least one uppercase letter, " +
-				"one lowercase letter, and one number",
-			"status": "Error",
-		})
+		utils.BadRequest(c, "Password must be at least 8 "+
+			"characters long and contain "+
+			"at least one uppercase letter, "+
+			"one lowercase letter, and one number")
+	}
+
+	checkUsername := config.DB.Where("username = ?", userModel.Username).First(&user)
+
+	if checkUsername.RowsAffected > 0 {
+		utils.BadRequest(c, "Username already exists")
+		return
+	}
+
+	checkEmail := config.DB.Where("email = ?", userModel.Email).First(&user)
+
+	if checkEmail.RowsAffected > 0 {
+		utils.BadRequest(c, "Email already exists")
+		return
+	}
+
+	if err := models.Validate.Struct(userModel); err != nil {
+		utils.BadRequest(c, err.Error())
 		return
 	}
 
@@ -74,19 +81,13 @@ func RegisterController(c *gin.Context) {
 	_, err := database.InsertUsers(users)
 
 	if err != nil {
-		// Return an HTTP error with details of the error
-		c.AbortWithStatusJSON(http.StatusInternalServerError, map[string]interface{}{
-			"code":    500,
-			"message": "Error registration users",
-			"status":  err.Error(),
-		})
+		utils.InternalServerError(c, err.Error())
 		return
-
 	}
 
 	c.JSON(http.StatusOK, gin.H{
 		"code":    200,
-		"message": "success register users",
+		"message": "Success register users",
 		"status":  "success",
 	})
 
